0e7d84b136bdb0a411150873a71614d0e202f341
Denniel Cat <deny.cat666@yandex.ru>
2020-10-02 14:50:25 +0300

    template tabs

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,23 @@ $(function (){
 		// autoplay: true,
 		// autoplayTimeout: 5000
 	});
+
+	const $tabsLinksTitle = $('.tabs__link-title');
+    const $tabsBlocksContent = $('.tabs__block-content');
+
+    $tabsLinksTitle.each(function(index) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $tabsBlocksContent.removeClass('visible')
+            $tabsLinksTitle.removeClass('tabs__link-title--active');
+            $(this).addClass('tabs__link-title--active');
+            $tabsBlocksContent.each(function (indexContent) {
+                console.log({item:  $(this), indexContent});
+                
+                if (index === indexContent) {
+                    $(this).addClass('visible')
+                }
+            })
+        })
+    })
 })
