1424108e8683a0f741fc17d471666ede04e42ea1
Denniel Cat <deny.cat666@yandex.ru>
2020-10-03 20:30:44 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,31 @@ $(function (){
 		items: 1, // А по-умолчанию 3
 		nav: true,
 		loop: true,
-		// autoplay: true,
-		// autoplayTimeout: 5000
-	});
+    });
 
-	const $tabsLinksTitle = $('.tabs__link-title');
-    const $tabsBlocksContent = $('.tabs__block-content');
 
-    $tabsLinksTitle.each(function(index) {
-        $(this).on('click', function(e) {
-            e.preventDefault();
-            $tabsBlocksContent.removeClass('visible')
-            $tabsLinksTitle.removeClass('tabs__link-title--active');
-            $(this).addClass('tabs__link-title--active');
-            $tabsBlocksContent.each(function (indexContent) {
-                console.log({item:  $(this), indexContent});
+
+
+    $('.tabs').each(function(index) {
+        const $tabsLinksTitle = $(this).find('.tabs__link-title');
+        const $tabsBlocksContent = $(this).find('.tabs__block-content');
+
+
+        $tabsLinksTitle.each(function(index) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+    
                 
-                if (index === indexContent) {
-                    $(this).addClass('visible')
-                }
+                $tabsBlocksContent.removeClass('visible')
+                $tabsLinksTitle.removeClass('tabs__link-title--active');
+                $(this).addClass('tabs__link-title--active');
+
+                
+                $tabsBlocksContent.each(function (indexContent) {
+                    if (index === indexContent) {
+                        $(this).addClass('visible')
+                    }
+                })
             })
         })
     })
